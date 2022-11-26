2b75ed43e1e08517614964d9c421051212416c99
pavel <pavelk.cherepanov@gmail.com>
2022-11-26 20:14:57 +0700

    add linkedarray

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        Integer[] ints = {1, 12, 4};
-//        Integer[] ints1 = {0,0,1};
-//        ints.equals(ints1);
-//        System.out.println(ints);
-//        for (int i = 0; i < ints.length; i++) {
-//            System.out.println(ints[i]);
-//        }
-//        System.out.println(ints1);
-//        for (int i = 0; i < ints1.length; i++) {
-//            System.out.println(ints1[i]);
-//        }
-//        DynamicArray<Integer> dynamicArray1 = new DynamicArray<>(ints);
-//        System.out.println(dynamicArray1);
-//        ArraysList
-//        System.
+
         Integer[] ints = {1,23,4};
 
         DynamicArray<Integer> integers = new DynamicArray<>(ints);
